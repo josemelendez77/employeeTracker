d8413a3ba12395d4509786b593faa4c5ecd037a3
josemelendez77 <141665270+josemelendez77@users.noreply.github.com>
2024-01-03 21:14:22 -0700

    updated ReadME

--- db/connection.js
+++ db/connection.js
@@ -2,13 +2,12 @@ const mysql = require("mysql2");
 
 require('dotenv').config()
 
-// Connect to database
 const db = mysql.createConnection(
   {
     host: "localhost",
-    // Your MySQL username
+
     user: process.env.DB_USER,
-    // Your MySQL password
+
     password: process.env.DB_PW,
     database: process.env.DB_NAME
   },
